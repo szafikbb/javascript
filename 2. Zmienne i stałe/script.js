a56db59ef376d2d0e5835829ebed1084220c3754
szafikbb <61661397+szafikbb@users.noreply.github.com>
2021-02-06 07:54:46 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,30 @@
+//zadanie 1
+
+const number = 5;
+console.log(number);
+
+const name = "Kacper";
+console.log(name);
+
+const nn = number + name;
+console.log(nn);
+
+
+const bool = true;
+console.log(bool);
+
+const special = null;
+console.log(special);
+
+//zadanie 2
+
+let num1 = 5;
+let num2 = 22;
+let sum = 0;
+sum = num1+num2;
+console.log(sum);
+
+//zadanie 3 
+
+let num;
+console.log(num)
