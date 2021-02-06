8192b65a4fc79bdef25baa01c5e80efdfc1a2d29
szafikbb <61661397+szafikbb@users.noreply.github.com>
2021-02-06 07:53:16 +0100

    Update script.js

--- 1.Początki/script.js
+++ 1.Początki/script.js
@@ -1,3 +1,22 @@
 //zad 1
 
 console.log("Kacper");
+
+//zad 2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// zad 3
+
+//poprawnie wyświetlone
+console.log("Hello World");
+
+    //error
+console.log(Hello World);
+
+    //syntax error
+console.log("Hello World);
+
+            
