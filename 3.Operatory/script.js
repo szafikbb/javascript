b1a5f34252ebf49dd5dbcc00058485c98b279d80
szafikbb <61661397+szafikbb@users.noreply.github.com>
2021-02-06 07:58:23 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,44 @@
+//zad 1
+
+let first = true;
+let second = false;
+console.log(true == false);
+
+//zad 2
+
+let num1 = 8;
+let num2 = 3;
+moduloResult = 0;
+moduloResult = num1%num2;
+console.log(moduloResult);
+
+//zad 3
+
+let str1 = "b";
+let str2 = "a";
+let stringsResult = "";
+stringsResult = str1 + str2;
+console.log(stringsResult)
+
+//zad 4
+
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString); //true
+console.log(someNumber === someString); //false 
+
+// zad 5
+
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//zad 6
+
+let num1 = 40;
+let num2 = 50;
+let result = null;
+result = num1 > num2;
+console.log(result);
